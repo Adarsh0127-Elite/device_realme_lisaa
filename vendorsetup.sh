e783c771a7bda2be96e03d78270f1fb6bce2ce79
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-01-29 15:17:46 +0000

    lisaa: Fixup Udfps

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,15 +3,10 @@
 echo "Applying frameworks/native patch"
 cd frameworks/native
 git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git
-git cherry-pick 366c673b57b640d81d7a79801d8bd359df4cb1f8
-git cherry-pick 1f9157df9aaba97d0ed6be1f7f657aa1939c9225
+git cherry-pick 16d8742a8f54c1d2826d576581bef688ba4a8655
+git cherry-pick 4f504aa24b143d3a6b4c21af114b9d733c975927
 cd ../..
 
 echo "Clonning IMS"
 git clone https://github.com/techyminati/android_vendor_mediatek_ims.git vendor/mediatek/ims
 
-echo "Applying frameworks/base patch"
-cd frameworks/base
-git fetch https://github.com/Adarsh0127-Elite/android_frameworks_base.git
-git cherry-pick 2f559990e06c6c2254d88ab3d3549fc8b1a8ba82
-cd ../..
