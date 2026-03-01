7150af0a18266d5f1c1567efef4aa59b2e73a413
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-01 23:02:11 +0530

    Revert "lisaa: Include Viper4AndroidFX"
    This reverts commit f5ef22d89f9a95789eca0e0180e0bfc7517920f5.

--- device.mk
+++ device.mk
@@ -366,8 +366,5 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.rtt.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.rtt.xml \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml
 
-# ViPER4AndroidFX
-$(call inherit-product, $(DEVICE_PATH)/viper/config.mk)
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/realme/lisaa/lisaa-vendor.mk)
