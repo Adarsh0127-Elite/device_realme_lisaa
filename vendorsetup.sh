19b8a51d4cc3591aa0781aee75030f886a3ee0aa
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-02 16:14:34 +0530

    Revert "Update patches"
    This reverts commit 6edb1d3f8a5d81cc98fdf802905efe46bdf9fcee.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,13 +1,1 @@
-#!/bin/bash
-
-echo "Applying frameworks/native patch 1"
-cd frameworks/native
-git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git
-git cherry-pick 366c673b57b640d81d7a79801d8bd359df4cb1f8
-cd ../..
-
-echo "Applying frameworks/native patch 2"
-cd frameworks/native
-git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git
-git cherry-pick 1f9157df9aaba97d0ed6be1f7f657aa1939c9225
-cd ../..
+cd frameworks/native && git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git && git cherry-pick 366c673b57b640d81d7a79801d8bd359df4cb1f8 && cd ../.. && cd frameworks/native && git cherry-pick 1f9157df9aaba97d0ed6be1f7f657aa1939c9225 && cd ../..
