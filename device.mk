aec14aebc3f7204f80def9257ab252c5e0ac72a8
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-02 17:09:52 +0530

    lisaa: Enable MTK GHBM udfps impl and define the DimLayer name

--- device.mk
+++ device.mk
@@ -136,6 +136,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
+$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set,surfaceflinger,mtk_dim_layer,NTFingerprintDimLayer)
+
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
 
@@ -242,7 +245,8 @@ PRODUCT_PACKAGES += \
     LineageSDKResTarget \
     LineageSettingsProviderResTarget \
     packages \
-    OplusDozeResTarget
+    OplusDozeResTarget \
+    LineageSystemUIResTarget
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
