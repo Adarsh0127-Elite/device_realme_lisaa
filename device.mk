0a10645e802fd61da77970e14a57eb7720efd7ad
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-02 16:07:27 +0530

    Revert "lisaa: Fixup Udfps"
    This reverts commit 17db81699c5731dcc84d9e628155043bf4e49971.

--- device.mk
+++ device.mk
@@ -136,7 +136,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/generic_ramdisk.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
 # Fingerprint
-$(call soong_config_set,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set_bool,surfaceflinger,has_mtk_udfps,true)
+$(call soong_config_set,surfaceflinger,mtk_dim_layer,OnScreenFingerprintPressedIcon)
 
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.3-service.oplus
@@ -244,7 +245,8 @@ PRODUCT_PACKAGES += \
     LineageSDKResTarget \
     LineageSettingsProviderResTarget \
     packages \
-    OplusDozeResTarget
+    OplusDozeResTarget \
+    LineageSystemUIResTarget
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
