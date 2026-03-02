ed1d86937476993361523bd6ebbfc360f123174c
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-03-02 16:15:47 +0530

    Revert "lisaa: fixup broken gpu and fingerprint flashbang"
    This reverts commit 38d1f072007c0ce27cd52fec520ef674d5e51065.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-cd frameworks/native && git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git && git cherry-pick 366c673b57b640d81d7a79801d8bd359df4cb1f8 && cd ../.. && cd frameworks/native && git cherry-pick 1f9157df9aaba97d0ed6be1f7f657aa1939c9225 && cd ../..
+cd frameworks/native && git fetch https://github.com/Adarsh0127-Elite/android_frameworks_native.git && git cherry-pick 374eaf203de5544e34a17ea1cb4d87e90ad72340 && cd ../..
